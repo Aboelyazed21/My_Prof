1e61fcc8240ecb7034d476918c417d3f69f3b317
ZEZO <nabwalyzydhatm@gmail.com>
2026-08-02 19:09:15 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/My_Prof/',
+  base: "/My_Prof/",
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+});
